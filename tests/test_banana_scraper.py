3eff3ddb85d399102c34bd9e740ac3040b00e41e
LucaDelBasso <lucadelbasso98@gmail.com>
2022-12-19 19:39:21 +0000

    its an array I guess

--- tests/test_banana_scraper.py
+++ tests/test_banana_scraper.py
@@ -11,9 +11,9 @@ def test_get_newest_bananas():
     assert response.status_code == 200
     assert 'bananas-current' in headers["Content-Disposition"]
 
-def test_ods_to_dictionary():
+def test_ods_to_array():
     ods_response = banana_scraper.get_newest_bananas()
-    cleaned_data = banana_scraper.test_ods_to_dictionary(ods_response)
+    cleaned_data = banana_scraper.test_ods_to_array(ods_response)
     assert type(cleaned_data) is list
     try:
         assert type(cleaned_data.pop()) is dict
