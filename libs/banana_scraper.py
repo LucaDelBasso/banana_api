692474b6a480cea5730ad2d04674bf7b40a39bad
LucaDelBasso <lucadelbasso98@gmail.com>
2022-12-19 19:07:46 +0000

    retrieving newest prices .ods

--- libs/banana_scraper.py
+++ libs/banana_scraper.py
@@ -2,11 +2,14 @@ import requests, re
 
 gov_url = 'https://www.gov.uk/government/statistical-data-sets/banana-prices'
 
-def get_all_data():
+def get_all_bananas():
     response = requests.get(gov_url)
     csv_url = re.search(r'https:\/\/.+\/bananas-.+csv', response.text)[0]
-    print(csv_url)
     csv_text = requests.get(csv_url).text
     return csv_text
 
-
+def get_newest_bananas():
+    response = requests.get(gov_url)
+    ods_url = re.search(r'https:\/\/.+\/bananas-.+ods', response.text)[0]
+    ods_response = requests.get(ods_url)
+    return ods_response
--- tests/test_banana_scraper.py
+++ tests/test_banana_scraper.py
@@ -1,4 +1,10 @@
 from libs import banana_scraper
 
-def test_get_all_data():
-    assert '£' in banana_scraper.get_all_data()
+def test_get_all_bananas():
+    assert '£' in banana_scraper.get_all_bananas()
+
+def test_get_newest_bananas():
+    response = banana_scraper.get_newest_bananas()
+    headers = response.headers
+    assert response.status_code == 200
+    assert 'bananas-current' in headers["Content-Disposition"]
